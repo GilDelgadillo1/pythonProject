ecd6f46c1fe4e96bf878224623c248fdd6c81951
Gilberto Delgadillo Sandoval <za16011346@zapopan.tecmm.edu.mx>
2020-09-22 00:12:41 -0500

    Escribir mi nombre

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -10,11 +7,11 @@ def print_hi(name):
 
 
 def hola():
-    print("como estamos todos")
+    print("como estas, Gilberto")
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     # print_hi('Gilberto')
     hola()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
