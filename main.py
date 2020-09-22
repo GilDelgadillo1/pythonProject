f9ee653614d6c5b583399e564db921128ca88dca
Gilberto Delgadillo Sandoval <za16011346@zapopan.tecmm.edu.mx>
2020-09-22 00:38:12 -0500

    suma

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def hola():
-    print("como estas, Gilberto")
-
-
+def suma():
+    num1= int(input("Dame un numero "))
+    num2=int(input("Dame otro numero "))
+    print("la suma es: ", num1+num2)
 
 if __name__ == '__main__':
     # print_hi('Gilberto')
-    hola()
+   suma()
 
